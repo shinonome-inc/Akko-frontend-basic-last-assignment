68df8c7cee96eb2f341ee0def85c669735ad83f4
akkomorita <a.morita.22.12.733@4nonome.com>
2023-01-21 10:48:55 +0900

    organisms/news作成

---
+++ src/stories/organisms-news.stories.js
@@ -0,0 +1,6 @@
+import Item_Plan from "../components/organisms/plan/plan.html";
+import "../components/organisms/plan/plan.scss";
+
+export default { title: "Organisms/plan" };
+
+export const Plan = () => Item_Plan;
